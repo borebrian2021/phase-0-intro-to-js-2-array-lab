09fa1b8c7e94d63f65f58496f75b9b389b643ffe
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-05 20:12:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+let cats=["Milo", "Otis", "Garfield"];
+// function destructivelyAppendCat(name){
+// beforeEach(function () {
+//     cats.length = 0;
+//     cats.push("Milo", "Otis", "Garfield");
+//   });
+// }
+function destructivelyAppendCat(name){
+  cats.push(name);
+}
+
+function  destructivelyPrependCat(name){
+  cats.unshift(name);
+  return cats;
+}
+
+function  destructivelyRemoveLastCat(){
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+  cats.shift();
+}
+
+function appendCat(name){
+  var newArray = cats.slice();
+  // or ES6 way
+  // var newArray = [...kittens];
+  newArray.push(name)
+  return newArray
+}
+function prependCat(name){
+  var cats2=[name, ...cats];
+  return cats2;
+}
+function removeLastCat(){
+  var cats2= cats.slice(0,cats.length-1);
+  return cats2;
+}
+function removeFirstCat(){
+  var cats2=cats.slice(1);
+  return cats2
+}
